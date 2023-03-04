18a5a868700be43760a5104b7aba283787462914
Bushra Siddiqui <soniasiddiqui@gmail.com>
2023-03-04 15:36:11 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('#searchForm');
+
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    removeImages();
+
+    const searchTerm = form.elements.query.value;
+   const config = { params: { q: searchTerm } }
+
+   const res =  await axios.get('https://api.tvmaze.com/search/shows', config);
+    
+   makeImages(res.data);
+   form.elements.query.value= "";
+
+
+})
+
+
+const makeImages = (shows)=>{
+      for(let result of shows){
+          if(result.show.image){
+                const img = document.createElement('IMG');
+                img.src = result.show.image.medium;
+                document.body.append(img);
+          }
+
+      }
+
+}
+
+const removeImages = ()=>{
+    const images = document.querySelectorAll('img');
+      for (let i of images) {
+          i.parentNode.removeChild(i);
+        }
+ }
